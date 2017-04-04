fe1ff7edce9f2b5fdb16a8fac972de8c6f927a5f
William D'Castro <william.i.dcastro@gmail.com>
2017-04-04 23:11:25 +0800

    Gallery and backlog feature implementations
    - Gallery now has thumbnails added to it
    - Need to make a directive to expand it
    - Backlog now display some lines. Needs to make an array to track which
    lines have been shown.

--- scripts/app.js
+++ scripts/app.js
@@ -9,7 +9,7 @@ app.controller('GameController', function(){
   this.showBacklog = false;
   this.lines = ["I hear Jerusalem bells a-ringing", "Roman calvary choirs are singing", "Be my mirror, my sword and shield", "My missionaries in a foreign field", "For some reason I can't explain", "I know St. Peter won't call my name"];
   this.imageChanges = [1,2,4];
-  this.images = ['res/images/pic1.jpg','res/images/pic2.jpg', 'res/images/pic3.jpg', 'res/images/pic1.jpg'];
+  this.images = ["res/images/pic1.jpg", "res/images/pic2.jpg", "res/images/pic3.jpg", "res/images/pic1.jpg"];
   this.currentLine = 0;
   this.currentImage = 0;
 
@@ -42,6 +42,14 @@ app.controller('GameController', function(){
       this.backgroundImage = this.images[this.currentImage];
     }
   };
+
+});
+
+app.controller('GalleryController', function(){
+  this.size = '10%';
+  this.expand = function(){
+  };
+
 
 });
 
